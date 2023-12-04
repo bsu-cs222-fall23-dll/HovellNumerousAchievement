f60da946fc0f0dc782a307921e7f045f2865529d
Camden Hovell <camdenhovell@gmail.com>
2023-12-04 00:10:48 -0500

    Refactor

--- src/main/java/edu/bsu/cs222/convertI.java
+++ src/main/java/edu/bsu/cs222/convertI.java
@@ -1,7 +1,11 @@
 package edu.bsu.cs222;
 
 public class convertI {
-    public String convertAll(int i) {
-        return "III";
+    public String convertAll(int input) {
+        String output = "";
+        for (int i = 1; i <= input; i++) {
+            output = output + ("I");
+        }
+        return output;
     }
 }

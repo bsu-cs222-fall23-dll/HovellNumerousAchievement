406eba7a1a1b454fe44fbeae4dd39018ba0fc30e
Camden Hovell <camdenhovell@gmail.com>
2023-12-04 00:24:31 -0500

    Refactor

--- src/main/java/edu/bsu/cs222/convertM.java
+++ src/main/java/edu/bsu/cs222/convertM.java
@@ -2,6 +2,12 @@ package edu.bsu.cs222;
 
 public class convertM {
     public String convertAll(int i) {
-        return "MM";
+        String output = "";
+        while (i >= 1000) {
+            output = output + "M";
+            i = i - 1000;
+        }
+
+        return output;
     }
 }
